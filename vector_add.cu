e8e7bb09475c7cb306c3ef39cf7fcebc83bec280
Nathan Breitsch <nathanbreitsch@gmail.com>
2015-10-12 23:31:53 -0400

    did stuff

--- vector_add.cu
+++ vector_add.cu
@@ -1,32 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-__global__ void addKernel(const double* x1, const double* x2, double result, int length){
+// questions:
+// 1. if I had a bunch of vectors to add in succession, would there be any benefit in keeping the memory allocated
+// and doing all the additions before freeing memory?  What would be the impact?
 
+// 2. given that thread creation has some overhead, would it make sense to do multiple additions per thread instead of one?  What is optimal num_add?
+
+// todo: modify this code and use timer to see which parameters are optimal for card 
+
+// 3. if i pass a device memory location into *result for cudaAdd, does it store the result on the card? (could probs google this, but... todo: try it out)
+
+
+__global__ void addKernel(double *x1, double*x2, double *result, int length){
+	//get unique index on which to compute
+	int index = threadIdx.x + blockIdx.x * blockDim.x;
+	
+	//add vectors on this index
+	if(index < length){
+		result[index] = x1[index] + x2[index];
+	}
 }
 
-void cudaAdd(const double* x1, const double* x2, double* result, int length){
+void cudaAdd(const double *x1, const double *x2, double *result, int length){
 	//set block size, i.e. number of threads in each block
-	int block_size = 1024;
+	int block_size = 512;
 	//set grid size, i.e. number of blocks
 	int grid_size = length / block_size + 1;
 	
 	//allocate some memory on the device
-	double* d_x1, d_x2, d_result;
-	int num_bytes = length*sizeof(double);
+	double *d_x1, *d_x2, *d_result;
+	
+	size_t num_bytes = length*sizeof(double);
 	cudaMalloc(&d_x1, num_bytes);
 	cudaMalloc(&d_x2, num_bytes);
 	cudaMalloc(&d_result, num_bytes);
 	
 	//copy data to the device
-	cudaMemcpy(&)
+	cudaMemcpy(&d_x1, &x1, num_bytes, cudaMemcpyHostToDevice);
+	cudaMemcpy(&d_x2, &x2, num_bytes, cudaMemcpyHostToDevice);
 	
-	//execute the kernel
+	//start the kernel
+	addKernel<<<grid_size, block_size>>>(d_x1, d_x2, d_result, length);
 	
 	//copy data back from the device
+	cudaMemcpy(d_result, result, num_bytes, cudaMemcpyDeviceToHost);
+	
+	//free memory
+	cudaFree(d_x1);
+	cudaFree(d_x2);
+	cudaFree(d_result);
+	
 }
 
-void serialAdd(const double* x1, const double* x2, double* result, int length){
+void serialAdd(const double *x1, const double *x2, double *result, int length){
 	for(int i = 0; i < length; i++){
 		result[i] = x1[i] + x2[i];
 	}
@@ -38,9 +65,9 @@ int main(){
 	
 	int size = 60;
 	int num_bytes = size * sizeof(double);
-	double* x1 = (double*) malloc(num_bytes);
-	double* x2 = (double*) malloc(num_bytes);
-	double* result = (double*) malloc(num_bytes);
+	double *x1 = (double*) malloc(num_bytes);
+	double *x2 = (double*) malloc(num_bytes);
+	double *result = (double*) malloc(num_bytes);
 	for(int i = 0; i < size; i++){
 		x1[i] = i;
 		x2[i] = i*i;
@@ -48,7 +75,7 @@ int main(){
 	
 	//call cudaAdd
 	
-	serialAdd(x1, x2, result, size);
+	cudaAdd(x1, x2, result, size);
 	
 	//print result
 	
